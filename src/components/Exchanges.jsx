881aa3175328ece71b525abcbb556d9f1fd5758c
Safwan <Safwanjaved24@gmail.com>
2024-08-15 19:18:50 +0530

    One more commit

--- src/components/Exchanges.jsx
+++ src/components/Exchanges.jsx
@@ -2,8 +2,7 @@
 import millify from 'millify';
 import { Collapse, Row, Col, Typography, Avatar } from 'antd';
 import HTMLReactParser from 'html-react-parser';
-
-import { useGetExchangesQuery } from '../services/cryptoApi';
+import { useGetExchangesQuery } from '../Services/CryptoApi';
 import Loader from './Loader';
 
 const { Text } = Typography;
